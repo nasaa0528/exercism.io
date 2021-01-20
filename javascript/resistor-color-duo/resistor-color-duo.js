9f0a08fd012dca0e26ad90e0a52fa230f2eae605
Nasantogtokh Amarsaikhan <nasaa@Nasantogtokhs-iMac.local>
2021-01-20 15:57:11 +0800

    color codes updated

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,13 +1,6 @@
 export const decodedValue = (colors) => {
-	var result = '';
 	if(colors.length > 2)
-		colors = colors.slice(0,2); 
-	for (const color of colors)
-	{
-		var decoded = COLOR_CODES.indexOf(color);
-		if (decoded !== -1) 
-			result += decoded; 
-	}
-	return Number(result); 
+		colors = colors.slice(0,2);
+  return +(colors.map(x => COLOR_CODES.indexOf(x))).join("");
 };
 const COLOR_CODES = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
